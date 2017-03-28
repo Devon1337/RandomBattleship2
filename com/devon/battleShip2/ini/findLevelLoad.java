04fb38b9cdaa8a0c7a3e25f48e795645d97e9f59
Devon <Sanitygamers@outlook.com>
2017-03-28 10:16:44 -0500

    Create findLevelLoad.java

---
+++ com/devon/battleShip2/ini/findLevelLoad.java
@@ -0,0 +1,20 @@
+package com.devon.battleShip2.ini;
+
+import com.devon.battleShip2.ini.*;
+
+public class findLevelLoad {
+	
+	public static MainMenu shipini = new MainMenu();
+
+	public void findLevels(char saveInput) {
+		if(saveInput == 'm') {
+			shipini.displayMenu();
+		}
+		
+	}
+	
+	public static void loadLevel() {
+		
+	}
+	
+}
