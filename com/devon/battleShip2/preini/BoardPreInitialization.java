6af655d3036f24a9fbb6bea23ff053d896f61d48
Devon <Sanitygamers@outlook.com>
2017-03-28 10:19:37 -0500

    Create BoardPreInitialization.java

---
+++ com/devon/battleShip2/preini/BoardPreInitialization.java
@@ -0,0 +1,20 @@
+package com.devon.battleShip2.preini;
+
+import com.devon.battleShip2.BattleshipTwo;
+
+public class BoardPreInitialization {
+	
+	public static BattleshipTwo bst = new BattleshipTwo();
+	
+	
+	
+	// setups the board for default game
+	public static void setupBoard() {
+		for(int x = 0; x < 10; x++) {
+			for(int y = 0; y < 10; y++) {
+				bst.hmBoard[x][y] = "[ ]";
+				bst.shBoard[x][y] = "[ ]";
+			}
+		}
+	}
+}
